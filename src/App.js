30ba187af33700762917877191f935e9f02093f9
sergeyCosilov <96079160+sergeyCosilov@users.noreply.github.com>
2022-03-21 00:58:09 +0300

    delete unnessery folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, Button, InputNumber, DatePicker } from 'antd';
 
 function App() {
+  async function onFinish (values) {
+    console.log('submit form')
+    const url = `https://tenders.hostman.site/api/tender-requests/}`
+    await fetch(url, {
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      values
+    })
+      .then((response) => response.json())
+      .then((res) => alert(res.data))
+      .catch((e) => {
+        console.log(e)
+        alert(e)
+      })
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Card Number"
+        name="cardNumber"
+        rules={[
+          {
+            required: true,
+            message: 'Please input Card Number!',
+          },
+          { type: 'number', max: 9999999999999999 }
+        ]}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item
+        label="Expiration Date"
+        name="expirationDate"
+        rules={[
+          {
+            required: true,
+            message: 'Please input Expiration Date!',
+          },
+        ]}
+      >
+        <DatePicker />
+      </Form.Item>
+
+      <Form.Item
+        label="CVV"
+        name="cvv"
+        rules={[
+          {
+            required: true,
+            message: 'Please input CVV!',
+          },
+          { type: 'number', max: 999 }
+        ]}
+      >
+        <InputNumber/>
+      </Form.Item>
+
+      <Form.Item
+        label="Amount"
+        name="amount"
+        rules={[
+          {
+            required: true,
+            message: 'Please input Amount!',
+          },
+          { type: 'number'}
+        ]}
+      >
+        <InputNumber min={1}/>
+      </Form.Item>
+
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
     </div>
   );
 }
